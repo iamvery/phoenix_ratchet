9c909903f35ed615185d868d37951ae8a0f0eabe
Jay Hayes <ur@iamvery.com>
2016-07-19 10:37:54 -0500

    Depend on released ratchet lib
    Whoops, this slipped in

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule PhoenixRatchet.Mixfile do
     [
       {:phoenix, "~> 1.1"},
       {:phoenix_html, "~> 2.5"},
-      {:ratchet, github: "iamvery/ratchet", branch: "data"},
+      {:ratchet, ">= 0.3.0"},
       {:ex_doc, ">= 0.0.0", only: :dev},
     ]
   end
