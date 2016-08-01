35bf854f71bd9ccc8012ab4871eccf900d036ef4
Jay Hayes <ur@iamvery.com>
2016-08-01 18:17:07 -0500

    Implement action macro
    At this point, it doesn't even appear to need to be a macro. That will
    come next...

--- lib/ratchet/action.ex
+++ lib/ratchet/action.ex
@@ -1,3 +1,11 @@
 defmodule Ratchet.Action do
   @moduledoc "TODO"
+
+  defmacro action(event, do: body) do
+    quote do
+      def unquote(event)(var!(conn)) do
+        unquote(body)
+      end
+    end
+  end
 end
--- test/ratchet/action_test.exs
+++ test/ratchet/action_test.exs
@@ -2,4 +2,26 @@ defmodule Ratchet.ActionTest do
   use ExUnit.Case, async: true
   alias Ratchet.Action
   doctest Action
+
+  defmodule Foo do
+    import Action
+
+    action :create do
+      conn.value
+    end
+  end
+
+  describe "create action" do
+    test "returns result of body" do
+      result = Foo.create(%{value: :foo})
+
+      assert result == :foo
+    end
+
+    @tag :skip
+    test "broadcasts data on endpoint"
+
+    @tag :skip
+    test "does not broadcast when result fails"
+  end
 end
