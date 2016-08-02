1cee0d1a781e7372910df669912a1e9632743834
Jay Hayes <ur@iamvery.com>
2016-08-01 19:08:21 -0500

    Implement Plug behaviour

--- lib/ratchet/plug/data.ex
+++ lib/ratchet/plug/data.ex
@@ -1,3 +1,18 @@
 defmodule Ratchet.Plug.Data do
   @moduledoc "TODO"
+
+  @behaviour Plug
+
+  def init(_opts), do: false
+
+  @doc """
+  Initialize connection data
+
+      iex> conn = %Plug.Conn{}
+      iex> Data.call(conn, [])
+      %Plug.Conn{assigns: %{data: %{}}}
+  """
+  def call(conn, _opts) do
+    update_in conn.assigns, &Map.put(&1, :data, %{})
+  end
 end
