d81a1c47ba3c85b10b53f97a04f8b49e865ae350
Jay Hayes <ur@iamvery.com>
2016-09-16 07:55:27 -0500

    Extract jank-related code from project
    See
    https://github.com/iamvery/phoenix_jank/commit/e0ca456382f973fe9a00da8898e05ceba2eb6cc6

--- mix.exs
+++ mix.exs
@@ -40,7 +40,6 @@ defmodule PhoenixRatchet.Mixfile do
     [
       {:phoenix, "~> 1.1"},
       {:phoenix_html, "~> 2.5"},
-      {:plug, "~> 1.1"},
       {:ratchet, ">= 0.3.0"},
       {:ex_doc, ">= 0.0.0", only: :dev},
     ]
